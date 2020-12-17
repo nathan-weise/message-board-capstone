7ac5841a3953add8ef19a72be15c0960c1758591
Kurt <kurt.russell.westfall@gmail.com>
2020-12-17 08:54:46 -0500

    Changes to the images in the database to add some variety

--- backend/database/data.sql
+++ backend/database/data.sql
@@ -18,9 +18,9 @@ VALUES ('Murtaugh', '$2a$10$U1/EBRALXy8OnnOaoCFr2e6Tzrh/6bLwy1hIdpIuKBXNWqIXEMNA
 
 -- Dummy data for forum table
 INSERT INTO forums (forum_name, forum_description, user_id, color, forum_image, created_time)
-VALUES ('Banana', 'Description', 1, '000000', 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
+VALUES ('Banana', 'Description', 1, '000000', 'https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg', TIMESTAMP '2020-12-14 10:00:00');
 INSERT INTO forums (forum_name, forum_description, user_id, color, forum_image, created_time)
-VALUES ('Baguette', 'Description', 2, '000000', 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
+VALUES ('Baguette', 'Description', 2, '000000', 'https://imgp2.schaer.com/sites/default/files/1998_Baguette%20Casera.jpg', TIMESTAMP '2020-12-14 10:00:00');
 INSERT INTO forums (forum_name, forum_description, user_id, color, forum_image, created_time)
 VALUES ('Breadstick', 'Description', 3, '000000', 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
 INSERT INTO forums (forum_name, forum_description, user_id, color, forum_image, created_time)
@@ -40,11 +40,11 @@ VALUES ('Multigrain', 'Description', 1, '000000', 'https://i.imgur.com/Frk3Kiy.j
 INSERT INTO forums (forum_name, forum_description, user_id, color, forum_image, created_time)
 VALUES ('Parrot', 'Description', 2, '000000', 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO forums (forum_name, forum_description, user_id, color, forum_image, created_time)
-VALUES ('Owl', 'Description', 3, '000000', 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-10 10:00:00');
+VALUES ('Owl', 'Description', 3, '000000', 'https://www.rd.com/wp-content/uploads/2018/02/32_Hilarious-Photos-that-Will-Get-You-Through-the-Week_481708660_Pitipat-Usanakornkul-760x506.jpg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO forums (forum_name, forum_description, user_id, color, forum_image, created_time)
 VALUES ('Hummingbird', 'Description', 1, '000000', 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO forums (forum_name, forum_description, user_id, color, forum_image, created_time)
-VALUES ('Duck', 'Description', 2, '000000', 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-10 10:00:00');
+VALUES ('Duck', 'Description', 2, '000000', 'https://www.allaboutbirds.org/guide/assets/photo/60021831-480px.jpg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO forums (forum_name, forum_description, user_id, color, forum_image, created_time)
 VALUES ('Goose', 'Description', 3, '000000', 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO forums (forum_name, forum_description, user_id, color, forum_image, created_time)
@@ -62,11 +62,11 @@ VALUES ('Swallow', 'Description', 2, '000000', 'https://i.imgur.com/Frk3Kiy.jpeg
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
 VALUES ('popular post', 'text', 1, 1, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
-VALUES ('popular post', 'text', 2, 2, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-10 10:00:00');
+VALUES ('popular post', 'text', 2, 2, 'https://www.thespruce.com/thmb/3icP-Tf1lTO_b-IPMOotdwNO_Uc=/1500x844/smart/filters:no_upscale()/mallard11-59511e483df78cae81214b68.jpg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
 VALUES ('popular post', 'text', 3, 3, 'https://i.imgur.com/N0JTeJi.jpeg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
-VALUES ('popular post', 'text', 1, 4, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-10 10:00:00');
+VALUES ('popular post', 'text', 1, 4, 'https://images.pexels.com/photos/260024/pexels-photo-260024.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
 VALUES ('popular post', 'text', 2, 5, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
@@ -74,29 +74,29 @@ VALUES ('popular post', 'text', 3, 6, 'https://i.imgur.com/N0JTeJi.jpeg', TIMEST
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
 VALUES ('popular post', 'text', 1, 7, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
-VALUES ('popular post', 'text', 2, 8, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-10 10:00:00');
+VALUES ('popular post', 'text', 2, 8, 'https://www.rd.com/wp-content/uploads/2018/02/25_Hilarious-Photos-that-Will-Get-You-Through-the-Week_280228817_Doty911.jpg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
 VALUES ('popular post', 'text', 3, 1, 'https://i.imgur.com/N0JTeJi.jpeg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
-VALUES ('popular post', 'text', 1, 2, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-10 10:00:00');
+VALUES ('popular post', 'text', 1, 2, 'https://www.dictionary.com/e/wp-content/uploads/2018/07/honey-badger3.jpg', TIMESTAMP '2020-12-10 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
-VALUES ('recent post', 'text', 2, 3, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
+VALUES ('recent post', 'text', 2, 3, 'https://s23527.pcdn.co/wp-content/uploads/2018/09/Mary-McGowan_Caught-In-The-Act_00001294.jpg.optimal.jpg', TIMESTAMP '2020-12-14 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
 VALUES ('recent post', 'text', 3, 4, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
-VALUES ('recent post', 'text', 1, 5, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
+VALUES ('recent post', 'text', 1, 5, 'https://www.rd.com/wp-content/uploads/2018/02/20_Hilarious-Photos-that-Will-Get-You-Through-the-Week_551201965_artemisphoto-760x506.jpg', TIMESTAMP '2020-12-14 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
 VALUES ('recent post', 'text', 2, 6, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
-VALUES ('recent post', 'text', 3, 7, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
+VALUES ('recent post', 'text', 3, 7, 'https://miro.medium.com/max/3200/1*EUffFv13pctf2d1ctR-XFw.png', TIMESTAMP '2020-12-14 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
-VALUES ('recent post', 'text', 1, 8, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
+VALUES ('recent post', 'text', 1, 8, 'https://us.123rf.com/450wm/kurashova/kurashova1811/kurashova181100020/112880849-cute-funny-guinea-pig-among-beautiful-pink-flowers-against-a-pink-background-.jpg?ver=6', TIMESTAMP '2020-12-14 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
-VALUES ('recent post', 'text', 2, 1, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
+VALUES ('recent post', 'text', 2, 1, 'https://www.thisiscolossal.com/wp-content/uploads/2015/12/WINNER-Julian-Rad.jpg', TIMESTAMP '2020-12-14 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
 VALUES ('recent post', 'text', 3, 2, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
-VALUES ('recent post', 'text', 1, 3, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
+VALUES ('recent post', 'text', 1, 3, 'https://i.barkpost.com/wp-content/uploads/2015/01/poem-option.jpg?q=70&fit=crop&crop=entropy&w=808&h=500', TIMESTAMP '2020-12-14 10:00:00');
 INSERT INTO posts (post_title, post_text, forum_id, user_id, post_image, created_time)
 VALUES ('recent post', 'text', 2, 4, 'https://i.imgur.com/Frk3Kiy.jpeg', TIMESTAMP '2020-12-14 10:00:00');
 
